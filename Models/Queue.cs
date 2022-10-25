863dbbf155965a9028a6f8d7024c1f1613b1acfe
Zumry <amzumry@gmail.com>
2022-10-25 21:09:52 +0530

    Creating Queue service

---
+++ Models/Queue.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace EcoGasBackend.Models
+{
+    public class Queue
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("stationID")]
+        public string? StationID { get; set; }
+        [BsonElement("userID")]
+        public string? UserID { get; set; }
+        [BsonElement("arrivalDate")]
+        public string? ArrivalDate { get; set; }
+        [BsonElement("arrivalTime")]
+        public string? ArrivalTime { get; set; }
+
+    }
+}
